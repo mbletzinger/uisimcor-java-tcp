e68974aae138699ab850ac5a4ad9e4e2600bbf41
mbletzin <mbletzin@d001af02-91e2-447c-bc6e-fea64fa78a0a>
2010-03-09 19:30:28 +0000

    Refactoring states

--- src/main/java/org/nees/uiuc/simcor/states/common/CheckListenerOpenConnection.java
+++ src/main/java/org/nees/uiuc/simcor/states/common/CheckListenerOpenConnection.java
@@ -1,4 +1,4 @@
-package org.nees.uiuc.simcor.states.listener;
+package org.nees.uiuc.simcor.states.common;
 
 import org.nees.uiuc.simcor.states.StateActionsProcessor;
 import org.nees.uiuc.simcor.states.TransactionState;
