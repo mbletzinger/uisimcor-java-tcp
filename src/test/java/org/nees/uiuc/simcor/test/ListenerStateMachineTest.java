b539b3bced3ef365dd811b9b73f753a61b68e200
mbletzin <mbletzin@d001af02-91e2-447c-bc6e-fea64fa78a0a>
2011-01-25 10:23:31 +0000

    Restructuring Tests

--- src/test/java/org/nees/uiuc/simcor/test/ListenerStateMachineTest.java
+++ src/test/java/org/nees/uiuc/simcor/test/ListenerStateMachineTest.java
@@ -16,8 +16,8 @@ import org.nees.uiuc.simcor.tcp.TcpError.TcpErrorTypes;
 import org.nees.uiuc.simcor.test.util.StateActionsResponder;
 import org.nees.uiuc.simcor.test.util.StateActionsResponder.DieBefore;
 
-public class T04_ListenerStateMachineTest  extends TestCase {
-	private final Logger log = Logger.getLogger(T04_ListenerStateMachineTest.class);
+public class ListenerStateMachineTest  extends TestCase {
+	private final Logger log = Logger.getLogger(ListenerStateMachineTest.class);
 	private TcpParameters lparams = new TcpParameters();
 	private ListenerStateMachine lsm;
 	private TcpParameters rparams = new TcpParameters();
--- src/test/java/org/nees/uiuc/simcor/test/TransactionTests.java
+++ src/test/java/org/nees/uiuc/simcor/test/TransactionTests.java
@@ -8,11 +8,11 @@ public class TransactionTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite(TransactionTests.class.getName());
 		//$JUnit-BEGIN$
-		suite.addTestSuite(T03_StateActionsTest.class);
-		suite.addTestSuite(T04_ListenerStateMachineTest.class);
-		suite.addTestSuite(T05_TransactionTest.class);
-		suite.addTestSuite(T06_TriggerTest.class);
-		suite.addTestSuite(T07_BroadcastTest.class);
+		suite.addTestSuite(StateActionsTest.class);
+		suite.addTestSuite(ListenerStateMachineTest.class);
+		suite.addTestSuite(TransactionTest.class);
+		suite.addTestSuite(TriggerTest.class);
+		suite.addTestSuite(BroadcastTest.class);
 		//$JUnit-END$
 		return suite;
 	}
