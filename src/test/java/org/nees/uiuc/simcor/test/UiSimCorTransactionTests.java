0dd4ba2ccbe5f9dd13661b9999cca811342ca4a1
mbletzin <mbletzin@d001af02-91e2-447c-bc6e-fea64fa78a0a>
2011-01-25 10:24:33 +0000

    Restructuring Tests

--- src/test/java/org/nees/uiuc/simcor/test/UiSimCorTransactionTests.java
+++ src/test/java/org/nees/uiuc/simcor/test/UiSimCorTransactionTests.java
@@ -3,10 +3,10 @@ package org.nees.uiuc.simcor.test;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-public class TransactionTests {
+public class UiSimCorTransactionTests {
 
 	public static Test suite() {
-		TestSuite suite = new TestSuite(TransactionTests.class.getName());
+		TestSuite suite = new TestSuite(UiSimCorTransactionTests.class.getName());
 		//$JUnit-BEGIN$
 		suite.addTestSuite(StateActionsTest.class);
 		suite.addTestSuite(ListenerStateMachineTest.class);
